1d3c565a780ef78d125c5d7406401b15bfe44047
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2023-07-08 10:56:29 -0700

    Upload all models

--- src/main/java/dev/epicpuppy/cloudtech/events/ServerEvents.java
+++ src/main/java/dev/epicpuppy/cloudtech/events/ServerEvents.java
@@ -1,8 +1,5 @@
 package dev.epicpuppy.cloudtech.events;
 
-import dev.epicpuppy.cloudtech.item.custom.RainbowCloudItem;
-import net.minecraftforge.event.TickEvent;
-
 public class ServerEvents {
 
 }
--- src/main/java/dev/epicpuppy/cloudtech/item/ICloudItem.java
+++ src/main/java/dev/epicpuppy/cloudtech/item/ICloudItem.java
@@ -3,5 +3,5 @@ package dev.epicpuppy.cloudtech.item;
 import net.minecraft.world.level.ItemLike;
 
 public interface ICloudItem extends ItemLike {
-    public int getColor(int pTintIndex);
+    int getColor(int pTintIndex);
 }
