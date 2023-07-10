1c1b96c77c7eae0331a802c116b30a0091bc3250
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2023-07-10 12:18:37 -0700

    Removed gear recipes and added loot tables to blocks

--- src/main/resources/data/cloudtech/loot_tables/blocks/model.js
+++ src/main/resources/data/cloudtech/loot_tables/blocks/model.js
@@ -5,8 +5,18 @@ const fs = require('fs');
 
 
 for (const color of COLORS) {
-    const loot = JSON.parse(fs.readFileSync("white_cloud_block.json"));
-    loot.pools[0].entries[0].children[0].name = "cloudtech:" + color + "_cloud_block";
-    loot.pools[0].entries[0].children[1].name = "cloudtech:" + color + "_cloud";
-    fs.writeFileSync(color + "_cloud_block.json", JSON.stringify(loot, null, 2));
+    const model = {};
+    model.type = "minecraft:block";
+    model.pools = [];
+    model.pools.push({});
+    model.pools[0].rolls = 1;
+    model.pools[0].bonus_rolls = 0;
+    model.pools[0].entries = [];
+    model.pools[0].entries.push({});
+    model.pools[0].entries[0].type = "minecraft:item";
+    model.pools[0].entries[0].name = "cloudtech:" + color + "_cloud_solidifier";
+    model.pools[0].conditions = [];
+    model.pools[0].conditions.push({});
+    model.pools[0].conditions[0].condition = "minecraft:survives_explosion";
+    fs.writeFileSync(color + "_cloud_solidifier.json", JSON.stringify(model, null, 4));
 }
